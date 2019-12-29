3303fd81127a2de9c43464df9aa729e33fea92b1
Andrew Weiss <wandrew8@gmail.com>
2019-12-29 12:51:28 -0800

    main components added along with styling

--- react-store/src/index.js
+++ react-store/src/index.js
@@ -1,2 +1,9 @@
-// let's go!
-alert("Let's do this!");
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
+
+
+
+render(<App />, document.querySelector('#main'));
